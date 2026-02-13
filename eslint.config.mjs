dad31de3e100567305b28257127fe10f858d15fb
Cousin Bastien <bastiencousin16@gmail.com>
2026-02-13 09:50:26 +0100

    Modification des règles du linter et formatage des fichiers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,43 @@
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     {
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+        },
         rules: {
             // Vos règles personnalisées
             eqeqeq: ["error", "always"],
             "prefer-const": ["error", { destructuring: "all" }],
-            "no-useless-rename": "error",
-            "no-useless-constructor": "error",
-            "no-console": "error",
+            "no-console": "off",
             "no-debugger": "error",
-            "no-throw-literal": "error",
             "no-unused-expressions": "error",
             "no-var": "error",
             "no-redeclare": "error",
+            "@typescript-eslint/no-empty-object-type": "off",
             "no-const-assign": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^",
+                    varsIgnorePattern: "^",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
+            "@typescript-eslint/no-explicit-any": [
+                "error",
+                { fixToUnknown: true },
+            ],
+
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
         },
     },
     {
-        ignores: ["node_modules", "dist", "build", "tests"],
+        ignores: ["node_modules", "dist", "build", "tests", "public"],
     },
 )
