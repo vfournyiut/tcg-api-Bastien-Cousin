637b40c4307484a4b8fc9ea93fb0dd7de3749f5f
Cousin Bastien <bastiencousin16@gmail.com>
2026-03-01 22:51:50 +0100

    Ajout du système d'authentification pour les connexions WebSocket

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import {createServer} from "http";
 import {env} from "./env";
 import express from "express";
 import cors from "cors";
@@ -7,9 +6,21 @@ import {cardsRouter} from "./cards.route";
 import {decksRouter} from "./decks.route";
 import swaggerUi from 'swagger-ui-express';
 import {swaggerDocument} from './docs';
+import {Server} from 'socket.io'
+import * as http from "node:http";
+import { authenticateSocket } from './auth.socket';
 
 // Create Express app
 export const app = express();
+
+export const server = http.createServer(app);
+export const io = new Server(server, {
+    cors: {
+        origin: '*',
+    },
+});
+
+io.use(authenticateSocket);
 
 // Middlewares
 app.use(
@@ -43,13 +54,9 @@ app.get("/api/health", (_req, res) => {
 
 // Start server only if this file is run directly (not imported for tests)
 if (require.main === module) {
-    // Create HTTP server
-    const httpServer = createServer(app);
-
-
     // Start server
     try {
-        httpServer.listen(env.PORT, () => {
+        server.listen(env.PORT, () => {
             console.log(`\n🚀 Server is running on http://localhost:${env.PORT}`);
             console.log(`🧪 Socket.io Test Client available at http://localhost:${env.PORT}`);
         });
